3e1bd48f5afe1f580cb4d36ee23066d9b1685e7e
Arena <dhiogoboza@gmail.com>
2017-12-16 09:09:23 -0300

    Adding tests

---
+++ test/tests.js
@@ -0,0 +1,14 @@
+// test if jQuery is available
+QUnit.test("jQuery test if exists", function(assert) {
+  assert.ok(jQuery, "jQuery window object exists");
+  assert.ok($, "jQuery's dollar object exists");
+  assert.ok($.fn, "jQuery's dollar fn function exists");
+});
+
+// test if $.fn.timeline is available
+QUnit.test("$.fn.autocomplete jquery function test if exists", function(assert) {
+  assert.ok($.fn.hotTrendsBubbles, "$.fn.autocomplete exists");
+});
+
+QUnit.todo("$.fn.autocomplete test", function(assert) {
+});
